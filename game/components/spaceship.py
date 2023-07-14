64093a08799596ef4476a50d70006fb7df7dc1ba
WendyFlorezP <wendyflorez403@gmail.com>
2023-07-14 07:21:25 -0500

    Tarea: borde de la nave y teclas simultanees
    Las teclas de movimiento hacia arriba, abajo, izquierda y derecha para mover la nave en cualquier dirección con ajuste de poder presionarlas al mismo tiempo y al verificar si la nave ha alcanzado los bordes de la pantalla y ajustar su posición, logramos que la nave salga por un lado y aparezca en el lado opuesto, creando así fluidez.
    Wendy Florez

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -13,24 +13,45 @@ class Spaceship(Sprite):
         self.rect.x = self.X_POS
         self.rect.y = self.Y_POS
 
+        self.moving_left = False
+        self.moving_right = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self, user_input):
-        if user_input[pygame.K_LEFT]:
-            self.move_left()
-        elif user_input[pygame.K_RIGHT]:
-            self.move_right()
-        elif user_input[pygame.K_DOWN]:
-            self.move_dowm()
-        elif user_input[pygame.K_UP]:
-            self.move_up()
+        if user_input[pygame.K_LEFT]:       
+            self.moving_left = True
+        else:
+            self.moving_left = False
+
+        if user_input[pygame.K_RIGHT]:
+            self.moving_right = True
+        else:
+            self.moving_right = False
+
+        if user_input[pygame.K_UP]:
+            self.moving_up = True
+        else:
+            self.moving_up = False
+
+        if user_input[pygame.K_DOWN]:
+            self.moving_down = True
+        else:
+            self.moving_down = False
+
+        self.move()
 
     def move_left (self):
         if self.rect.left > 0:
             self.rect.x -= 10
+        elif self.rect.left <= 0:
+            self.rect.x = SCREEN_WIDTH - self.rect.width
 
     def move_right(self):
         if self.rect.right < SCREEN_WIDTH:
-            self.rect.x += 10 
+            self.rect.x += 10
+        elif self.rect.right >= SCREEN_WIDTH:
+            self.rect.x = 0
 
     def move_dowm(self):
         if self.rect.bottom < SCREEN_HEIGHT:
@@ -40,6 +61,20 @@ class Spaceship(Sprite):
         if self.rect.top > SCREEN_HEIGHT // 2:
             self.rect.y -= 10
 
+    def move(self):
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= 10
+        elif self.moving_right and self.rect.right < SCREEN_WIDTH:
+            self.rect.x += 10
+        elif self.moving_right and self.rect.right >= SCREEN_WIDTH:
+            self.rect.x = 0
+        elif self.moving_left and self.rect.left <= 0:
+            self.rect.x = SCREEN_WIDTH - self.rect.width
+            
+        if self.moving_up and self.rect.top > 0:
+            self.rect.y -= 10
+        elif self.moving_down and self.rect.bottom < SCREEN_HEIGHT:
+            self.rect.y += 10
 
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
